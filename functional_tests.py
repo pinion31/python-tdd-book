b2c8dcaf5c7e21793ce3fe012892aea4fd59c638
Chris Cantu <pinion31@gmail.com>
2019-03-31 21:44:59 -0500

    Models for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,12 @@ class NewVisitorTest(unittest.TestCase):
   def setUp(self):
     self.browser = webdriver.Firefox()
 
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text , [row.text for row in rows])
+
+
   def tearDown(self):
     self.browser.quit()
 
@@ -25,16 +31,17 @@ class NewVisitorTest(unittest.TestCase):
     )
 
     inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use a peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
 
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_element_by_tag_name('tr')
-    self.assertTrue(
-      any(row.text == '1: Buy peacock feathers' for row in rows),
-      "New to-do item did not appear in table"
-    )
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
+    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
     self.fail('Finish the test!')
 
