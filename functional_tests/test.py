452827e0eb79a06b68d8a29f2702b8c8d0497ef8
Chris Cantu <pinion31@gmail.com>
2019-04-01 22:43:21 -0500

    added sqllite db, test passing

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,10 +1,12 @@
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
   def setUp(self):
     self.browser = webdriver.Firefox()
 
@@ -18,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     self.browser.quit()
 
   def test_can_start_a_list_and_retrieve_it_later(self):
-    self.browser.get('http://localhost:8000')
+    self.browser.get(self.live_server_url)
 
     self.assertIn('To-Do',self.browser.title)
     header_text = self.browser.find_element_by_tag_name('h1').text
@@ -32,19 +34,23 @@ class NewVisitorTest(unittest.TestCase):
 
     inputbox.send_keys('Buy peacock feathers')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+    #time.sleep(1)
+    WebDriverWait(self.browser, 10).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), 'Buy peacock feathers'))
+
     self.check_for_row_in_list_table('1: Buy peacock feathers')
 
     inputbox = self.browser.find_element_by_id('id_new_item')
-    inputbox.send_keys('Use a peacock feathers to make a fly')
+    inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+    #time.sleep(1)
+    WebDriverWait(self.browser, 10).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), 'Use peacock feathers to make a fly'))
 
     self.check_for_row_in_list_table('1: Buy peacock feathers')
     self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-    self.fail('Finish the test!')
+    #self.fail('Finish the test!')
 
-
-if __name__ == '__main__':
-  unittest.main(warnings='ignore')
