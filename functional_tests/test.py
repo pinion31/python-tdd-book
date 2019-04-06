a47fd5dae521f5054177f29c687149f3475df84b
Chris Cantu <pinion31@gmail.com>
2019-04-06 18:39:15 -0500

    First steps of FT for layout and styling

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -19,6 +19,67 @@ class NewVisitorTest(LiveServerTestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def wait_for_row_in_list_table(self, row_text):
+    WebDriverWait(self.browser, 10).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), row_text))
+
+  def test_layout_and_styling(self):
+    self.browser.get(self.live_server_url)
+    self.browser.set_window_size(1024, 768)
+
+
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('testing')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('testing')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width'] / 2,
+      512,
+      delta=10
+    )
+"""
+  def test_can_start_a_list_for_one_user(self):
+    self.browser.get(self.live_server_url)
+    inputbox = self.browser.find_element_by_id('id_new_item')
+
+    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('Buy peacock feathers')
+
+  def test_multiple_users_can_start_lists_at_different_urls(self):
+    self.browser.get(self.live_server_url)
+    inputbox = self.browser.find_element_by_id('id_new_item')
+
+    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('Buy peacock feathers')
+
+    edith_list_url = self.browser.current_url
+    self.assertRegex(edith_list_url, '/lists/.+')
+
+    self.browser.quit()
+    self.browser = webdriver.Firefox()
+
+    self.browser.get(self.live_server_url)
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy peacock feathers', page_text)
+    self.assertNotIn('make a fly', page_text)
+
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Buy milk')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('Buy milk')
+
+    francis_list_url = self.browser.current_url
+    self.assertRegex(francis_list_url, '/lists/.+')
+    self.assertNotEqual(francis_list_url, edith_list_url)
+
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy peacock feathers', page_text)
+    self.assertIn('Buy milk', page_text)
+
   def test_can_start_a_list_and_retrieve_it_later(self):
     self.browser.get(self.live_server_url)
 
@@ -35,9 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys('Buy peacock feathers')
     inputbox.send_keys(Keys.ENTER)
     #time.sleep(1)
-    WebDriverWait(self.browser, 10).until(
-            expected_conditions.text_to_be_present_in_element(
-                (By.ID, 'id_list_table'), 'Buy peacock feathers'))
+    self.wait_for_row_in_list_table('Buy peacock feathers')
 
     self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -45,12 +104,10 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
     #time.sleep(1)
-    WebDriverWait(self.browser, 10).until(
-            expected_conditions.text_to_be_present_in_element(
-                (By.ID, 'id_list_table'), 'Use peacock feathers to make a fly'))
+    self.wait_for_row_in_list_table('Use peacock feathers to make a fly')
 
     self.check_for_row_in_list_table('1: Buy peacock feathers')
     self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
+"""
     #self.fail('Finish the test!')
 
