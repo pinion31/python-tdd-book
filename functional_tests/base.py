984a1123076fa88a725d0b3d7b8d532d98a0cd09
Chris Cantu <pinion31@gmail.com>
2019-04-13 20:57:49 -0500

    update tests

---
+++ functional_tests/base.py
@@ -0,0 +1,36 @@
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+import time
+from unittest import skip
+
+MAX_WAIT=10
+
+class FunctionalTest(StaticLiveServerTestCase):
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    staging_server = os.environ.get('STAGING_SERVER')
+    if staging_server:
+      self.live_server_url = 'http://' + staging_server
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def wait_for_row_in_list_table(self, row_text):
+    WebDriverWait(self.browser, 10).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), row_text))
+
+  def wait_for(self, fn):
+    start_time = time.time()
+    while True:
+      try:
+        return fn()
+      except (AssertionError, WebDriverException) as e:
+        if time.time() - start_time > MAX_WAIT:
+          raise e
+        time.sleep(0.5)
---
+++ functional_tests/test_simple_list_creation.py
@@ -0,0 +1,44 @@
+from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class NewVisitorTest(FunctionalTest):
+  def test_can_start_a_list_for_one_user(self):
+    self.browser.get(self.live_server_url)
+    inputbox = self.browser.find_element_by_id('id_new_item')
+
+    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('Buy peacock feathers')
+
+  def test_multiple_users_can_start_lists_at_different_urls(self):
+    self.browser.get(self.live_server_url)
+    inputbox = self.browser.find_element_by_id('id_new_item')
+
+    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('Buy peacock feathers')
+
+    edith_list_url = self.browser.current_url
+    self.assertRegex(edith_list_url, '/lists/.+')
+
+    self.browser.quit()
+    self.browser = webdriver.Firefox()
+
+    self.browser.get(self.live_server_url)
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy peacock feathers', page_text)
+    self.assertNotIn('make a fly', page_text)
+
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Buy milk')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('Buy milk')
+
+    francis_list_url = self.browser.current_url
+    self.assertRegex(francis_list_url, '/lists/.+')
+    self.assertNotEqual(francis_list_url, edith_list_url)
+
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy peacock feathers', page_text)
+    self.assertIn('Buy milk', page_text)
