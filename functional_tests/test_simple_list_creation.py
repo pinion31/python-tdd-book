4f5045414b971a2e143d8b577fbe1e69790beae9
Chris Cantu <pinion31@gmail.com>
2019-04-20 16:44:21 -0500

    Update

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(FunctionalTest):
   def test_can_start_a_list_for_one_user(self):
     self.browser.get(self.live_server_url)
-    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox = get_item_input_box()
 
     inputbox.send_keys('Buy peacock feathers')
     inputbox.send_keys(Keys.ENTER)
@@ -13,7 +13,7 @@ class NewVisitorTest(FunctionalTest):
 
   def test_multiple_users_can_start_lists_at_different_urls(self):
     self.browser.get(self.live_server_url)
-    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox = get_item_input_box()
 
     inputbox.send_keys('Buy peacock feathers')
     inputbox.send_keys(Keys.ENTER)
@@ -30,7 +30,7 @@ class NewVisitorTest(FunctionalTest):
     self.assertNotIn('Buy peacock feathers', page_text)
     self.assertNotIn('make a fly', page_text)
 
-    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox = get_item_input_box()
     inputbox.send_keys('Buy milk')
     inputbox.send_keys(Keys.ENTER)
     self.wait_for_row_in_list_table('Buy milk')
