79f164606c08a7744289271c80ca0a807fb9aee7
Chris Cantu <pinion31@gmail.com>
2019-04-21 19:22:13 -0500

    4-21 update

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,7 +4,7 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
-def test_cannot_add_empty_list_items(self):
+  def test_cannot_add_empty_list_items(self):
     # Edith goes to the home page and accidentally tries to submit
     # an empty list item. She hits Enter on the empty input box
     self.browser.get(self.live_server_url)
@@ -12,13 +12,13 @@ def test_cannot_add_empty_list_items(self):
     # The browser intercepts the request, and does not load the
     # list page
     self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-        '#id_text:invalid'
-    ))
+      '#id_text:invalid'
+      ))
     # She starts typing some text for the new item and the error disappears
     self.get_item_input_box().send_keys('Buy milk')
     self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-        '#id_text:valid'
-    ))
+      '#id_text:valid'
+      ))
     # And she can submit it successfully
     self.get_item_input_box().send_keys(Keys.ENTER)
     self.wait_for_row_in_list_table('1: Buy milk')
@@ -27,18 +27,18 @@ def test_cannot_add_empty_list_items(self):
     # Again, the browser will not comply
     self.wait_for_row_in_list_table('1: Buy milk')
     self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-        '#id_text:invalid'
-    ))
+      '#id_text:invalid'
+      ))
     # And she can correct it by filling some text in
     self.get_item_input_box().send_keys('Make tea')
     self.wait_for(lambda: self.browser.find_aelements_by_css_selector(
-        '#id_text:valid'
-    ))
+      '#id_text:valid'
+      ))
     self.get_item_input_box().send_keys(Keys.ENTER)
     self.wait_for_row_in_list_table('1: Buy milk')
     self.wait_for_row_in_list_table('2: Make tea')
 
-def test_cannot_add_duplicate_items(self):
+  def test_cannot_add_duplicate_items(self):
     # Edith goes to the home page and starts a new list
     self.browser.get(self.live_server_url)
     self.get_item_input_box().send_keys('Buy wellies')
@@ -49,6 +49,23 @@ def test_cannot_add_duplicate_items(self):
     self.get_item_input_box().send_keys(Keys.ENTER)
     # She sees a helpful error message
     self.wait_for(lambda: self.assertEqual(
-    self.browser.find_element_by_css_selector('.has-error').text,
-    "You've already got this in your list"
-    ))
+      self.browser.find_element_by_css_selector('.has-error').text,
+      "You've already got this in your list"
+      ))
+
+  def test_error_messages_are_cleared_on_input(self):
+    # Edith starts a list and causes a validation error:
+    self.browser.get(self.live_server_url)
+    self.get_item_input_box().send_keys('Banter too thick')
+    self.get_item_input_box().send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('1: Banter too thick')
+    self.get_item_input_box().send_keys('Banter too thick')
+    self.get_item_input_box().send_keys(Keys.ENTER)
+    self.wait_for(lambda: self.assertTrue(
+      self.browser.find_element_by_css_selector('.has-error').is_displayed()
+      ))
+    # She starts typing in the input box to clear the error
+    self.get_item_input_box().send_keys('a')
+    # She is pleased to see that the error message disappears
+    self.wait_for(lambda: self.assertFalse(
+      self.browser.
